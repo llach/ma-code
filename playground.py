7148eb7bdc7f1f4d5dde4befbe4dbb6818131b35
llach <llach@techfak.uni-bielefeld.de>
2019-02-13 14:41:15 +0100

    rl.envs: merged atari envs, moved all env related things into submodule

---
+++ playground.py
@@ -0,0 +1,28 @@
+from forkan.rl import make
+import numpy as np
+import matplotlib.pyplot as plt
+
+# environment parameters
+env_conf = {
+    'id': 'Breakout-v0',
+    'frameskip': 4,
+    'num_frames': 4,
+    'num_envs': 3,
+    'obs_type': 'image',
+    'target_shape': (200, 160),
+}
+
+e = make(**env_conf)
+
+obs = e.reset()
+
+print(obs.shape)
+
+for _ in range(20):
+    obs = e.step(1)
+    obs = obs[0]
+    for i in range(obs.shape[-1]):
+
+        plt.imshow(obs[..., i], cmap='Greys_r')
+        plt.show()
+
