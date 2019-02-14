ad4169f7c0c3e3fd0d0284f744c1730add7f564d
llach <llach@techfak.uni-bielefeld.de>
2019-02-14 13:29:15 +0100

    ignoring test files

--- plotplay.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-np.random.seed(0)
-random_numbers = np.random.rand(100000)
-x = -np.log(-np.log(np.random.rand(100000)))
-# x = np.log(random_numbers / (1 - random_numbers))
-n, bins, patches = plt.hist(x, 50, density=True, facecolor='g', alpha=0.75)
-plt.grid(True)
-plt.show()
