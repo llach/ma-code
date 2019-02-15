4587f40073c186feea2bf51d46a5847f6573f02b
llach <llach@techfak.uni-bielefeld.de>
2019-02-15 10:31:11 +0100

    added a2c-pong

--- rl-test/a2c-pong.py
+++ rl-test/a2c-pong.py
@@ -1,30 +1,31 @@
 import argparse
 import logging
 
-from forkan.rl import make, DQN
+from forkan.rl import make, A2C
 
-dqn_conf = {
-    'name': 'dqb-breakout',
-    'network_type': 'nature-cnn',
-    'buffer_size': 1e6,
-    'total_timesteps': 5e7,
-    'training_start': 5e4,
-    'target_update_freq': 1e4,
-    'exploration_fraction': 0.1,
-    'gamma': .99,
-    'batch_size': 32,
-    'prioritized_replay': True,
-    'double_q': True,
-    'dueling': True,
+# algorithm parameters
+a2c_conf = {
+    'name': 'a2c-pong',
+    'policy_type': 'mnih-2013',
+    'total_timesteps': 1e6,
+    'lr': 5e-4,
+    'gamma': .95,
+    'entropy_coef': 0.01,
+    'v_loss_coef': 0.2,
+    'gradient_clipping': None,
+    'reward_clipping': None,
+    'use_tensorboard': True,
     'clean_tensorboard_runs': True,
     'clean_previous_weights': True,
+    'print_freq': 10,
 }
 
 # environment parameters
 env_conf = {
-    'id': 'Breakout-v0',
+    'id': 'Pong-v0',
     'frameskip': 4,
     'num_frames': 4,
+    'num_envs': 16,
     'obs_type': 'image',
     'target_shape': (110, 84),
     'crop_ranges': [(17, 103), (4, 80)]
@@ -39,14 +40,15 @@ log = logging.getLogger(__name__)
 
 # remove keys from config so that the correct environment will be created
 if args.run:
-    dqn_conf['clean_previous_weights'] = False
+    env_conf.pop('num_envs')
+    a2c_conf['clean_previous_weights'] = False
 
 e = make(**env_conf)
-alg = DQN(e, **dqn_conf)
+alg = A2C(e, **a2c_conf)
 
 if args.run:
-    log.info('Running dqn on {}'.format(env_conf['id']))
+    log.info('Running a2c on {}'.format(env_conf['id']))
     alg.run()
 else:
-    log.info('Learning with dqn on {}'.format(env_conf['id']))
+    log.info('Learning with a2c on {}'.format(env_conf['id']))
     alg.learn()
