fb895067133931045cd422d3d049cb5d3205fe61
llach <llach@techfak.uni-bielefeld.de>
2019-02-22 13:36:29 +0100

    added vae training script for dsprites translational

---
+++ vae/train/translation.py
@@ -0,0 +1,7 @@
+from forkan.models import VAE
+from forkan.datasets.dsprites import load_dsprites
+
+
+(data, _) = load_dsprites('translation', repetitions=10)
+v = VAE(data.shape[1:], name='dtrans', network='dsprites', beta=30.1, latent_dim=5)
+v.train(data, num_episodes=50, print_freq=20)
