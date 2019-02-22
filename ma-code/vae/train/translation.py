a615ea6ca6d74d18179f2e1b752b851374823960
llach <llach@techfak.uni-bielefeld.de>
2019-02-22 16:53:17 +0100

    vae: using higgins parameters

--- ma-code/vae/train/translation.py
+++ ma-code/vae/train/translation.py
@@ -3,5 +3,5 @@ from forkan.datasets.dsprites import load_dsprites
 
 
 (data, _) = load_dsprites('translation', repetitions=10)
-v = VAE(data.shape[1:], name='dtrans', network='dsprites', beta=30.1, latent_dim=5)
+v = VAE(data.shape[1:], name='dtrans', network='dsprites', beta=4.1, latent_dim=10, lr=1e-2)
 v.train(data, num_episodes=50, print_freq=20)
