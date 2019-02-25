e6523b1e23ab2ed95029c4a92336cf60bc7587b2
llach <llach@techfak.uni-bielefeld.de>
2019-02-25 21:53:39 +0100

    vae-train: script for beta search

---
+++ ma-code/vae/train/beta_search.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+from forkan.models import VAE
+from forkan.datasets import load_atari_normalized
+
+learning_rate = 1e-4
+latents = 20
+
+betas = [0.5, 1.0, 2.0, 5.0, 10.0, 22.5, 50.6]
+
+game = 'boxing'
+
+for beta in betas:
+    data = load_atari_normalized(game)
+    v = VAE(data.shape[1:], name=game, lr=learning_rate, beta=beta, latent_dim=latents)
+    v.train(data, num_episodes=50, print_freq=20)
+
+    tf.reset_default_graph()
+    del data
+    del v
