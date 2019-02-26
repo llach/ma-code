50673d8e4554ef52246eea933d70c9287d8f738c
llach <llach@techfak.uni-bielefeld.de>
2019-02-26 18:13:58 +0100

    passing optimizer to vae, trans: using calculated beta

--- macode/vae/train/beta_search_trans.py
+++ macode/vae/train/beta_search_trans.py
@@ -4,14 +4,15 @@ from forkan.models import VAE
 from forkan.datasets.dsprites import load_dsprites
 
 learning_rate = 1e-2
-latents = 5
+latents = 10
+opt = tf.train.AdagradOptimizer
 
-betas = [0.5, 1.0, 2.0, 5.0, 10.0, 22.5, 50.6]
+betas = [28.67, 40.96, 81.92] # also use crossent
 
 (data, _) = load_dsprites('translation', repetitions=10)
 
 for beta in betas:
-    v = VAE(data.shape[1:], name='trans', lr=learning_rate, beta=beta, latent_dim=latents)
+    v = VAE(data.shape[1:], name='trans', lr=learning_rate, beta=beta, latent_dim=latents, optimizer=opt)
     v.train(data, num_episodes=50, print_freq=-1)
 
     tf.reset_default_graph()
