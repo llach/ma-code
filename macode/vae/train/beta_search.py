4eb218624345c2063f2127f11a02bc935a8d59c9
llach <llach@techfak.uni-bielefeld.de>
2019-02-26 11:04:56 +0100

    beta-search: new hyperparams

--- macode/vae/train/beta_search.py
+++ macode/vae/train/beta_search.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from forkan.models import VAE
 from forkan.datasets import load_atari_normalized
 
-learning_rate = 1e-4
+learning_rate = 1e-3
 latents = 20
 
 betas = [0.5, 1.0, 2.0, 5.0, 10.0, 22.5, 50.6]
@@ -13,7 +13,7 @@ game = 'boxing'
 for beta in betas:
     data = load_atari_normalized(game)
     v = VAE(data.shape[1:], name=game, lr=learning_rate, beta=beta, latent_dim=latents)
-    v.train(data, num_episodes=50, print_freq=20)
+    v.train(data, num_episodes=100, print_freq=20)
 
     tf.reset_default_graph()
     del data
