e9efd7ce465add9500df37779493a86c8c7dc6b4
llach <llach@techfak.uni-bielefeld.de>
2019-03-01 08:54:32 +0100

    more episodes for beta search trans

--- macode/vae/train/beta_search_trans.py
+++ macode/vae/train/beta_search_trans.py
@@ -1,22 +1,16 @@
-import tensorflow as tf
-
 from forkan.models import VAE
 from forkan.datasets.dsprites import load_dsprites
 
-learning_rate = 1e-2
 latents = 10
-opt = tf.train.AdagradOptimizer
+opt = 'adam'
 
-# betas = [28.67, 40.96, 81.92]
-betas = [40.96]
+betas = [28.67, 40.96, 81.92]
 
 for beta in betas:
     (data, _) = load_dsprites('translation', repetitions=10)
 
-    v = VAE(data.shape[1:], name='trans', network='dsprites', lr=learning_rate, beta=beta,
-            latent_dim=latents, optimizer=opt, tensorboard=False)
-    v.train(data, num_episodes=50, print_freq=-1)
+    v = VAE(data.shape[1:], name='trans', network='dsprites', beta=beta, latent_dim=latents, optimizer=opt)
+    v.train(data, num_episodes=100)
 
-    tf.reset_default_graph()
     del data
     del v
