37d4856397523a78aa89e1f04b6713080f3edfda
llach <llach@techfak.uni-bielefeld.de>
2019-03-06 15:30:37 +0100

    vae: expanding dim and more prints

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from forkan.models import VAE
 from forkan.datasets import load_pendulum
 
@@ -7,8 +9,10 @@ betas = [0.5, 1.5, 2.0, 3.5, 5.0]
 
 for lr in lrs:
     for beta in betas:
+        print('loading data ...')
         data = load_pendulum()
-
+        data = np.expand_dims(data, -1)
+        print('starting training!')
         v = VAE(data.shape[1:], network='pendulum', name='', beta=beta, lr=lr, latent_dim=latents)
         v.train(data, num_episodes=100)
 
