5f707f1d79f08be841c0c08bb027f9b8b4be3ea3
llach <llach@techfak.uni-bielefeld.de>
2019-03-06 15:32:55 +0100

    vae: smaller searchspace for vae

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -5,7 +5,7 @@ from forkan.datasets import load_pendulum
 
 latents = 5
 lrs = [1e-3, 1e-4]
-betas = [0.5, 1.5, 2.0, 3.5, 5.0]
+betas = [1.5, 2.0]
 
 for lr in lrs:
     for beta in betas:
@@ -14,7 +14,7 @@ for lr in lrs:
         data = np.expand_dims(data, -1)
         print('starting training!')
         v = VAE(data.shape[1:], network='pendulum', name='', beta=beta, lr=lr, latent_dim=latents)
-        v.train(data, num_episodes=100)
+        v.train(data, num_episodes=50)
 
         del data
         del v
