1f14b66356759147a7d2d7c6b306ffdbbc05812e
llach <llach@techfak.uni-bielefeld.de>
2019-03-06 18:05:02 +0100

    pendulum: new betas 2

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -3,7 +3,7 @@ from forkan.datasets import load_pendulum
 
 latents = 5
 lr = 1e-3
-betas = [3.0, 4.0, 5.0, 6.0]
+betas = [6.0, 8.0, 10.0, 12.0, 20.0]
 
 for beta in betas:
     print('loading data ...')
