2120fe1a1aa4d06bcb3db9a30788772f04eebc10
llach <llach@techfak.uni-bielefeld.de>
2019-03-07 22:15:36 +0100

    a2c pendulum with VAE: more timesteps and ability to play

--- macode/pipe/a2c-vae-pend.py
+++ macode/pipe/a2c-vae-pend.py
@@ -1,12 +1,14 @@
 from baselines.run import main
 
 args = [
-    '--num_timesteps', '1e6',
+    '--num_timesteps', '5e6',
     '--env', 'Pendulum-v0',
     '--alg', 'a2c',
     '--network', 'mlp',
     '--num_env', '1',
-    '--vae', 'pend'
+    '--vae', 'pend',
+    # '--load_path', '/Users/llach/.forkan/models/a2c/pendulum-noVAE-nenv16-2019-03-07T21:56/weights_latest',
+    # '--play'
 ]
 
 main(args)
