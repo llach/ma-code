5acae4317b94a2825b297f383414e5fe54b1ea73
llach <llach@techfak.uni-bielefeld.de>
2019-03-11 12:31:36 +0100

    pendulum_stack: one env without VAE, appending left

--- macode/rl/a2c-pend.py
+++ macode/rl/a2c-pend.py
@@ -2,7 +2,7 @@ from baselines.run import main
 
 args = [
     '--num_timesteps', '10e6',
-    '--env', 'PendulumTheta-v0',
+    '--env', 'PendulumThetaStack-v0',
     '--alg', 'a2c',
     '--network', 'mlp',
     '--num_env', '24',
