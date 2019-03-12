89a371137191182902d45acc1692f1bc2fc566f4
llach <llach@techfak.uni-bielefeld.de>
2019-03-12 17:13:54 +0100

    pendulumtheta: using 3 last sin(th) and cos(th) and hyperparameter search not crashing

--- macode/rl/a2c-pend.py
+++ macode/rl/a2c-pend.py
@@ -1,6 +1,10 @@
+from keras.backend import clear_session
 from baselines.run import main
+from baselines.common.tf_util import get_session
+import tensorflow as tf
 
-for zahl in ['3', '4', '5']:
+
+for zahl in ['3', '4', '5', '16', '24']:
     args = [
         '--num_timesteps', '10e6',
         '--env', 'PendulumThetaStack-v0',
@@ -9,3 +13,10 @@ for zahl in ['3', '4', '5']:
         '--num_env', zahl,
     ]
     main(args)
+
+    print('done')
+    s = get_session()
+    s.close()
+    clear_session()
+    tf.reset_default_graph()
+    print('clear')
