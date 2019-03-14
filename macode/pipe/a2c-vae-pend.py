2beb8b4d13d0d48ea6a2c4cd480f1c4719acc1c9
llach <llach@techfak.uni-bielefeld.de>
2019-03-14 22:35:11 +0100

    introducint VecVAEStack: better pipelining with VAE as preprocessor for observations

--- macode/pipe/a2c-vae-pend.py
+++ macode/pipe/a2c-vae-pend.py
@@ -1,29 +1,23 @@
 from baselines.run import main
-from baselines.common.cmd_util import make_dummy_vec_env
+from baselines.common.cmd_util import make_vec_env
+from forkan.rl import VecVAEStack
 
-from forkan.rl import PendulumRenderEnv, PendulumVAEStackEnv
-
+vae_name = 'pend-optimal'
 
 def build_pend_env(args, **kwargs):
     alg = args.alg
     seed = args.seed
 
-    def wrap_fn(env):
-        env = PendulumRenderEnv(env)
-        env = PendulumVAEStackEnv(env, k=20)
-        return env
-
     flatten_dict_observations = alg not in {'her'}
-    return make_dummy_vec_env('Pendulum-v0', 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                             flatten_dict_observations=flatten_dict_observations, wrapper_fn=wrap_fn)
-
+    env = make_vec_env('PendulumVisual-v0', 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                             flatten_dict_observations=flatten_dict_observations)
+    return VecVAEStack(env, k=3, load_from=vae_name)
 
 args = [
     '--num_timesteps', '10e6',
-    '--env', 'PendulumVAEStack-v0',
     '--alg', 'a2c',
     '--network', 'mlp',
-    '--num_env', '6',
+    '--num_env', '16',
 ]
 
 main(args, build_fn=build_pend_env)
