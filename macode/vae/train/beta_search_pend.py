d3702ceb5efffdc14382e43e4590f7ebb995b612
llach <llach@techfak.uni-bielefeld.de>
2019-03-14 13:10:06 +0100

    new name for vae trained on new environment

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -9,7 +9,7 @@ for beta in betas:
     print('loading data ...')
     data = load_pendulum()
     print('starting training!')
-    v = VAE(data.shape[1:], network='pendulum', name='pend', beta=beta, lr=lr, latent_dim=latents)
+    v = VAE(data.shape[1:], network='pendulum', name='pendvisual', beta=beta, lr=lr, latent_dim=latents)
     v.train(data, num_episodes=100)
 
     del data
