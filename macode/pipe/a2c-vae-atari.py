da10c62bc6732b66fe7ec54979135f86f1292fd0
llach <llach@techfak.uni-bielefeld.de>
2019-03-15 09:36:28 +0100

    a2c-vae-atari: using VecVAEStack

--- macode/pipe/a2c-vae-atari.py
+++ macode/pipe/a2c-vae-atari.py
@@ -1,4 +1,18 @@
 from baselines.run import main
+from baselines.common.cmd_util import make_vec_env
+from forkan.rl import VecVAEStack
+
+vae_name = 'gopher'
+
+def build_pend_env(args, **kwargs):
+    alg = args.alg
+    seed = args.seed
+
+    flatten_dict_observations = alg not in {'her'}
+    env = make_vec_env(args.env, 'atari', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                             flatten_dict_observations=flatten_dict_observations)
+    return VecVAEStack(env, k=4, load_from=vae_name)
+
 
 args = [
     '--num_timesteps', '50e6',
@@ -6,7 +20,6 @@ args = [
     '--alg', 'a2c',
     '--num_env', '16',
     '--network', 'mlp',
-    '--vae', 'gopher',
 ]
 
 main(args)
