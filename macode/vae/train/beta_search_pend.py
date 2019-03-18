eb10b980e06357a091dbed72674ddc9060a7bd55
llach <llach@techfak.uni-bielefeld.de>
2019-03-18 19:49:20 +0100

    vae: fixed warmup callback checking

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -7,7 +7,7 @@ latents = 5
 lr = 1e-3
 betas = [0.5, 1.0, 2, 4, 20, 22, 30, 40, 50]
 
-for wu, bn in [(20, True), (None, False)]:
+for wu, bn in [(None, False)]:
     for beta in betas:
         print('loading data ...')
         data = load_uniform_pendulum()
