c60aed99655d8cdb5ab02f89f14906c5b5facff7
llach <llach@techfak.uni-bielefeld.de>
2019-03-18 16:29:37 +0100

    vae: trying warumup and batch norm

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -5,13 +5,14 @@ from forkan.datasets import load_uniform_pendulum
 
 latents = 5
 lr = 1e-3
-betas = np.linspace(10, 20, 13)
+betas = [0.5, 1.0, 2, 4, 20, 22, 30, 40, 50]
 
 for beta in betas:
     print('loading data ...')
     data = load_uniform_pendulum()
     print('starting training!')
-    v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents)
+    v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents,
+            warmup=20, batch_norm=True)
     v.train(data, num_episodes=100)
 
     del data
