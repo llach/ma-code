ebb0d6c08290276553f52401834557a4f03350f2
llach <llach@techfak.uni-bielefeld.de>
2019-03-20 22:27:04 +0100

    new beta search for pendulumvisualuniform

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -5,16 +5,14 @@ from forkan.datasets import load_uniform_pendulum
 
 latents = 5
 lr = 1e-3
-betas = [0.5, 1.0, 2, 4, 20, 22, 30, 40, 50]
+betas = 40+2.5*np.arange(17)
 
-for wu in [True, False]:
-    for beta in betas:
-        print('loading data ...')
-        data = load_uniform_pendulum()
-        print('starting training!')
-        v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents,
-                warmup=wu)
-        v.train(data, num_episodes=100)
+for beta in betas:
+    print('loading data ...')
+    data = load_uniform_pendulum()
+    print('starting training!')
+    v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents)
+    v.train(data, num_episodes=100)
 
-        del data
-        del v
+    del data
+    del v
