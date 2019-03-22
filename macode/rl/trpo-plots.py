f9d019cac765370490f339edfbee32bca16d4ab9
llach <llach@techfak.uni-bielefeld.de>
2019-03-22 09:59:48 +0100

    trpo-plots: only plotting 2M line if we have that much steps

--- macode/rl/trpo-plots.py
+++ macode/rl/trpo-plots.py
@@ -8,12 +8,16 @@ from forkan.common.utils import ls_dir
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
 
+import matplotlib as mlp
+
+# print(mlp.rcParams)
+
 log = logging.getLogger(__name__)
 
 models_dir = '{}trpo/'.format(model_path)
 dirs = ls_dir(models_dir)
 
-filter = ''
+filter = '16'
 for d in dirs:
     ds_name = d.split('/')[-1].split('-')[0]
     model_name = d.split('/')[-1]
@@ -63,38 +67,39 @@ for d in dirs:
 
     steps = np.asarray(steps, dtype=np.float32)
 
+    second_line = steps[-1] > 19e5
     fig, axes = plt.subplots(2, 2, figsize=(12, 10))
     fig.suptitle('{}'.format(model_name), fontsize=14)
 
+    # sns.set_context("paper", font_scale=1.5, rc={"lines.linewidth": 2.5})
+
     print('plotting losses for {} ...'.format(model_name))
     axes[0, 0].axvline(1e6, linestyle='--', color='c')
-    axes[0, 0].axvline(2e6, linestyle='--', color='c')
+    if second_line: axes[0, 0].axvline(2e6, linestyle='--', color='c')
     axes[0, 0].plot(steps, np.asarray(rew, dtype=np.float32), label='mean reward')
     # axes[0, 0].legend()
     axes[0, 0].set_title('reward')
 
     axes[0, 1].axvline(1e6, linestyle='--', color='c')
-    axes[0, 1].axvline(2e6, linestyle='--', color='c')
+    if second_line: axes[0, 1].axvline(2e6, linestyle='--', color='c')
     axes[0, 1].plot(steps, np.asarray(entr, dtype=np.float32), label='policy entropy')
     # axes[0, 1].legend()
     axes[0, 1].set_title('policy entropy')
 
     axes[1, 0].axvline(1e6, linestyle='--', color='c')
-    axes[1, 0].axvline(2e6, linestyle='--', color='c')
+    if second_line: axes[1, 0].axvline(2e6, linestyle='--', color='c')
     axes[1, 0].plot(steps, np.asarray(ev, dtype=np.float32), label='explained variance')
     axes[1, 0].set_title('explained variance')
 
     axes[1, 1].axvline(1e6, linestyle='--', color='c')
-    axes[1, 1].axvline(2e6, linestyle='--', color='c')
+    if second_line: axes[1, 1].axvline(2e6, linestyle='--', color='c')
     axes[1, 1].plot(steps, np.asarray(vall, dtype=np.float32), label='value loss')
     axes[1, 1].set_title('value loss')
-
-    # plt.setp([p.get_xticklabels() for p in axes[0,...]], visible=False)
 
     fig.tight_layout()
     fig.subplots_adjust(hspace=0.88)
 
-    # plt.savefig('{}/plot.png'.format(d))
+    plt.savefig('{}/plot.png'.format(d))
     plt.show()
 
 log.info('Done.')
