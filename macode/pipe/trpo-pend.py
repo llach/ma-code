cf053a49c50ba0fc0e0eda697b8e5267da3a6440
llach <llach@techfak.uni-bielefeld.de>
2019-03-22 10:01:00 +0100

    hardcoding latent indices or trpo PendulumVisual

--- macode/pipe/trpo-pend.py
+++ macode/pipe/trpo-pend.py
@@ -2,18 +2,18 @@ from baselines.run import main
 from baselines.common.cmd_util import make_env
 from forkan.rl import VAEStack
 
-vae_name = 'pendvisualuniform-b1.0-lat5-lr0.001-2019-03-18T19/56'.replace('/', ':')
+vae_name = 'pendvisualuniform-b72.5-lat5-lr0.001-2019-03-20T23/58'.replace('/', ':')
 
 def build_pend_env(args, **kwargs):
     seed = args.seed
 
     env = make_env(args.env, 'classic_control', seed=seed)
-    # return VAEStack(env, k=3, load_from=vae_name)
+    return VAEStack(env, k=3, load_from=vae_name)
     return env
 
 args = [
-    '--num_timesteps', '2e6',
-    '--env', 'PendulumTheta-v0',
+    '--num_timesteps', '5e6',
+    '--env', 'PendulumVisual-v0',
     '--alg', 'trpo_mpi',
     '--network', 'mlp',
 ]
