9eb94909ff5d0a9547c64ef3eab30e16837ceab2
llach <llach@techfak.uni-bielefeld.de>
2019-03-26 00:54:58 +0100

    using lstm in a2c and differekt K

--- macode/pipe/a2c-vae-pend.py
+++ macode/pipe/a2c-vae-pend.py
@@ -5,36 +5,30 @@ from baselines.run import main
 from baselines.common.cmd_util import make_vec_env
 from forkan.rl import VecVAEStack
 
-vaes = [
-    'pendvisualuniform-b77.5-lat5-lr0.001-2019-03-21T00/13'.replace('/', ':'),
-    'pendvisualuniform-b1.0-lat5-lr0.001-2019-03-18T19/56'.replace('/', ':')
-]
-for nenvs in [16, 12]:
-    for vae_name in vaes:
-        for lr in [1e-1, 1e-2, 1e-3, 1e-4, 5e-4, 1e-5]:
 
-            def build_pend_env(args, **kwargs):
-                alg = args.alg
-                seed = args.seed
+for nenvs in [16, 12, 8, 6]:
+    for k in [1, 4, 6]:
+        def build_pend_env(args, **kwargs):
+            alg = args.alg
+            seed = args.seed
 
-                flatten_dict_observations = alg not in {'her'}
-                env = make_vec_env('PendulumVisual-v0', 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                                         flatten_dict_observations=flatten_dict_observations)
-                return VecVAEStack(env, k=3, load_from=vae_name)
+            flatten_dict_observations = alg not in {'her'}
+            env = make_vec_env('PendulumVisual-v0', 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                                     flatten_dict_observations=flatten_dict_observations)
+            return VecVAEStack(env, k=k, load_from='pendvisualuniform-b77.5-lat5-lr0.001-2019-03-21T00/13'.replace('/', ':'))
 
-            args = [
-                '--env', 'PendulumVisual-v0',
-                '--num_timesteps', '8e6',
-                '--alg', 'a2c',
-                '--network', 'mlp',
-                '--num_env', str(nenvs),
-                '--lr', str(lr),
-                '--seed', '1',
-            ]
+        args = [
+            '--env', 'PendulumVisual-v0',
+            '--num_timesteps', '8e6',
+            '--alg', 'a2c',
+            '--network', 'lstm',
+            '--num_env', str(nenvs),
+            '--seed', '1',
+        ]
 
-            main(args, build_fn=build_pend_env)
-            s = get_session()
-            s.close()
-            clear_session()
-            tf.reset_default_graph()
-            pass
+        main(args, build_fn=build_pend_env)
+        s = get_session()
+        s.close()
+        clear_session()
+        tf.reset_default_graph()
+        pass
