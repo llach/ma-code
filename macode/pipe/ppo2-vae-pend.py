3176d06ae788889f58aab64c3f2af3e11f2e7065
llach <llach@techfak.uni-bielefeld.de>
2019-03-27 17:08:42 +0100

    logging k in ppo2

--- macode/pipe/ppo2-vae-pend.py
+++ macode/pipe/ppo2-vae-pend.py
@@ -6,30 +6,32 @@ from baselines.common.cmd_util import make_vec_env
 from forkan.rl import VecVAEStack
 
 
-def build_pend_env(args, **kwargs):
-    alg = args.alg
-    seed = args.seed
+for k in [3, 5]:
+    def build_pend_env(args, **kwargs):
+        alg = args.alg
+        seed = args.seed
 
-    flatten_dict_observations = alg not in {'her'}
-    env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                             flatten_dict_observations=flatten_dict_observations)
-    return VecVAEStack(env, k=3, load_from='pendvisualuniform-b77.5-lat5-lr0.001-2019-03-21T00/13'.replace('/', ':'))
+        flatten_dict_observations = alg not in {'her'}
+        env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                                 flatten_dict_observations=flatten_dict_observations)
+        return VecVAEStack(env, k=k, load_from='pendvisualuniform-b77.5-lat5-lr0.001-2019-03-21T00/13'.replace('/', ':'))
 
-args = [
-    '--env', 'PendulumVisual-v0',
-    '--num_timesteps', '10e6',
-    '--alg', 'ppo2',
-    '--network', 'mlp',
-    '--log_interval', '2',
-    '--nminibatches', '32',
-    '--noptepochs', '10',
-    '--num_env', '8',
-    '--seed', '1',
-]
+    args = [
+        '--env', 'PendulumVisual-v0',
+        '--num_timesteps', '10e6',
+        '--alg', 'ppo2',
+        '--network', 'mlp',
+        '--log_interval', '2',
+        '--nminibatches', '32',
+        '--noptepochs', '10',
+        '--num_env', '8',
+        '--seed', '1',
+        '--k', str(k),
+    ]
 
-main(args, build_fn=build_pend_env)
-s = get_session()
-s.close()
-clear_session()
-tf.reset_default_graph()
-pass
+    main(args, build_fn=build_pend_env)
+    s = get_session()
+    s.close()
+    clear_session()
+    tf.reset_default_graph()
+    pass
