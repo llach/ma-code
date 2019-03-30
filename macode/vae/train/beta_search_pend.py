ad4666098218616ebfd7150643c4e7ac9d6b6e46
llach <llach@techfak.uni-bielefeld.de>
2019-03-30 20:54:50 +0100

    ignoring session when storing vae params

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -15,7 +15,7 @@ for beta in np.append([1, 22.5], betas):
 
     with tf.Session() as s:
         v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform',
-                beta=beta, lr=lr, latent_dim=latents, sess=s)
+                beta=beta, lr=lr, latent_dim=latents, session=s)
         v.train(data, num_episodes=100, print_freq=200)
 
     tf.reset_default_graph()
