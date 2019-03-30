1340b7e5107b985fd2c00b298280527890416187
llach <llach@techfak.uni-bielefeld.de>
2019-03-30 19:54:36 +0100

    learning more betas

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -5,13 +5,14 @@ from forkan.datasets import load_uniform_pendulum
 
 latents = 5
 lr = 1e-3
+betas = 70+2.5*np.arange(5)
 
-for beta in [1, 22, 80]:
+for beta in np.append([1, 22.5], betas):
     print('loading data ...')
     data = load_uniform_pendulum()
     print('starting training!')
     v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents)
-    v.train(data, num_episodes=100)
+    v.train(data, num_episodes=100, print_freq=200)
 
     del data
     del v
