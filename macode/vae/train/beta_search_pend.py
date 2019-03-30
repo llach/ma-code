04ba871497809aeb0f151d1c3bb11a70e8cad92c
llach <llach@techfak.uni-bielefeld.de>
2019-03-30 20:52:30 +0100

    resetting graph after each run; using own session per graph

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 from forkan.models import VAE
 from forkan.datasets import load_uniform_pendulum
@@ -11,8 +12,13 @@ for beta in np.append([1, 22.5], betas):
     print('loading data ...')
     data = load_uniform_pendulum()
     print('starting training!')
-    v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform', beta=beta, lr=lr, latent_dim=latents)
-    v.train(data, num_episodes=100, print_freq=200)
+
+    with tf.Session() as s:
+        v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform',
+                beta=beta, lr=lr, latent_dim=latents, sess=s)
+        v.train(data, num_episodes=100, print_freq=200)
+
+    tf.reset_default_graph()
 
     del data
     del v
