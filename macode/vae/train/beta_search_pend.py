e4dfcd5c1d333072224ef33a5fd17cf2ef369569
llach <llach@techfak.uni-bielefeld.de>
2019-04-01 12:43:55 +0200

    gave this search an own name

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -6,7 +6,6 @@ from forkan.datasets import load_uniform_pendulum
 
 latents = 5
 lr = 1e-3
-betas = 70+2.5*np.arange(5)
 
 for beta in np.round(np.linspace(85, 95, 15), 2):
     print('loading data ...')
@@ -14,7 +13,7 @@ for beta in np.round(np.linspace(85, 95, 15), 2):
     print('starting training!')
 
     with tf.Session() as s:
-        v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniform',
+        v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniformONE',
                 beta=beta, lr=lr, latent_dim=latents, session=s)
         v.train(data, batch_size=128, num_episodes=50, print_freq=200)
 
