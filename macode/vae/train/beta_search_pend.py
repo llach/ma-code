ebafecb8975ff16d459d5a6e11498dc44edba629
llach <llach@techfak.uni-bielefeld.de>
2019-04-03 14:14:46 +0200

    searching through different seeds and other beta range

--- macode/vae/train/beta_search_pend.py
+++ macode/vae/train/beta_search_pend.py
@@ -15,62 +15,70 @@ lr = 1e-3
 
 logger = logging.getLogger(__name__)
 
-for beta in np.round(np.linspace(70, 110, 400), 2):
-    print('loading data ...')
-    data = load_uniform_pendulum()
-    print('starting training!')
+for seed in np.arange(10):
+    for beta in np.round(np.linspace(65, 90, 150), 2):
 
-    with tf.Session() as s:
-        v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniformONE',
-                beta=beta, lr=lr, latent_dim=latents, session=s)
-        v.train(data, batch_size=128, num_episodes=50, print_freq=200)
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
 
-    v.csv.flush()
-    d = '{}/vae-pendulum/{}/'.format(model_path, v.savename)
+        print('loading data ...')
+        data = load_uniform_pendulum()
+        print('starting training!')
 
-    ds_name = d.split('/')[-1].split('-')[0]
-    model_name = d.split('/')[-1]
+        with tf.Session() as s:
+            v = VAE(data.shape[1:], network='pendulum', name='pendvisualuniformONE',
+                    beta=beta, lr=lr, latent_dim=latents, session=s)
+            v.train(data, batch_size=128, num_episodes=50, print_freq=200)
 
-    print(d)
-    if not os.path.isfile('{}progress.csv'.format(d)):
-        logger.info('skipping {} | file not found'.format(model_name))
-        continue
+        v.csv.flush()
+        d = '{}/vae-pendulum/{}/'.format(model_path, v.savename)
 
-    zidx, zs = [], []
+        ds_name = d.split('/')[-1].split('-')[0]
+        model_name = d.split('/')[-1]
 
-    with open('{}progress.csv'.format(d)) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                for n, ele in enumerate(row):
-                    if '-kl' in ele:
-                        zidx.append(n)
+        print(d)
+        if not os.path.isfile('{}progress.csv'.format(d)):
+            logger.info('skipping {} | file not found'.format(model_name))
+            continue
 
-                for _ in zidx:
-                    zs.append([])
+        zidx, zs = [], []
 
-                line_count += 1
-            else:
+        with open('{}progress.csv'.format(d)) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line_count = 0
+            for row in csv_reader:
+                if line_count == 0:
+                    for n, ele in enumerate(row):
+                        if '-kl' in ele:
+                            zidx.append(n)
 
-                for n, idx in enumerate(zidx):
-                    zs[n].append(row[idx])
+                    for _ in zidx:
+                        zs.append([])
 
-                line_count += 1
+                    line_count += 1
+                else:
 
-    if zs[0] == []:
-        print('{} had no progress, skipping'.format(model_name))
-        continue
+                    for n, idx in enumerate(zidx):
+                        zs[n].append(row[idx])
 
-    zs = np.asarray(zs, dtype=np.float32)
-    zm = np.mean(zs, axis=-1)
-    num_z = len(zm[np.where(zm > 0.2)])
+                    line_count += 1
 
-    if num_z != 1:
-        print(f'removing {model_path} bc zm = {num_z}')
-        rmtree(d)
+        if zs[0] == []:
+            print('{} had no progress, skipping'.format(model_name))
+            continue
 
-    tf.reset_default_graph()
+        for r in range(len(zidx)):
+            zs[r] = zs[r][(line_count//2):]
 
-    del data
-    del v
+        zs = np.asarray(zs, dtype=np.float32)
+        zm = np.mean(zs, axis=-1)
+        num_z = len(zm[np.where(zm > 0.2)])
+
+        if num_z != 1:
+            print(f'removing {model_path} bc zm = {num_z}')
+            rmtree(d)
+
+        tf.reset_default_graph()
+
+        del data
+        del v
