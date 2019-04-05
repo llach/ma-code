0c71042d41553eaae865ae1fd005e63600df35fe
llach <llach@techfak.uni-bielefeld.de>
2019-04-05 16:13:04 +0200

    experimenting with 16 envsˆ

--- macode/rl/ppo2-pend.py
+++ macode/rl/ppo2-pend.py
@@ -1,8 +1,10 @@
+import tensorflow as tf
 from keras.backend import clear_session
+
 from baselines.common.cmd_util import make_vec_env
 from baselines.common.tf_util import get_session
-import tensorflow as tf
 from baselines.run import main
+
 
 def build_pend_env(args, **kwargs):
     return make_vec_env(args.env, 'classic_control', args.num_env or 1, args.seed, reward_scale=args.reward_scale,
@@ -16,7 +18,7 @@ args = [
     '--network', 'mlp',
     '--nminibatches', '32',
     '--noptepochs', '10',
-    '--num_env', '8',
+    '--num_env', '16',
     '--log_interval', '2',
     '--seed', '1',
     '--tensorboard', 'True',
