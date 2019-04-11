42938134b4572563223460e378cb8eca064e6cb7
llach <llach@techfak.uni-bielefeld.de>
2019-04-11 17:16:24 +0200

    some testing

--- macode/rl/ppo2-quicktest.py
+++ macode/rl/ppo2-quicktest.py
@@ -1,20 +1,11 @@
 import tensorflow as tf
-from keras.backend import clear_session
 
 from baselines.common.cmd_util import make_vec_env
 from baselines.common.tf_util import get_session
 from baselines.common.vec_env.vec_frame_stack import VecFrameStack
 from baselines.run import main
 
-nsteps = 10
-k = 3
-
-vae_params = {
-    'init_from': 'pendvisualuniform-b85.63-lat5-lr0.001-2019-04-06T02:14'.replace('/', ':'),
-    'k': k,
-    'latent_dim': 5,
-    'with_kl': True
-}
+k = 5
 
 def build_pend_env(args, **kwargs):
     alg = args.alg
@@ -25,25 +16,32 @@ def build_pend_env(args, **kwargs):
                        flatten_dict_observations=flatten_dict_observations)
     return VecFrameStack(env, k)
 
-args = [
-    '--env', 'PendulumTest-v0',
-    '--num_timesteps', '8e6',
-    '--alg', 'ppo2',
-    '--network', 'mlp',
-    '--nsteps', '10',
-    '--nminibatches', '2',
-    '--noptepochs', '3',
-    '--nsteps', '10',
-    '--num_env', '8',
-    '--log_interval', '2',
-    '--seed', '0',
-    '--tensorboard', 'True',
-    '--k', str(k),
-]
 
-main(args, build_fn=build_pend_env, vae_params=vae_params)
-s = get_session()
-s.close()
-clear_session()
-tf.reset_default_graph()
-print('done')
+for seed in [0, 1, 2]:
+    vae_params = {
+        'init_from': 'pendvisualuniform-b1-lat5-lr0.001-2019-04-08T22/04'.replace('/', ':'),
+        'k': k,
+        'latent_dim': 5,
+        'with_attrs': True,
+    }
+
+    args = [
+        '--env', 'PendulumVisual-v0',
+        '--num_timesteps', '10e6',
+        '--alg', 'ppo2',
+        '--network', 'mlp',
+        '--log_interval', '2',
+        '--nminibatches', '32',
+        '--noptepochs', '10',
+        '--num_env', '16',
+        '--seed', str(seed),
+        '--tensorboard', 'True',
+        '--k', str(k),
+        '--log_weights', 'True',
+    ]
+
+    main(args, build_fn=build_pend_env, vae_params=None)
+    s = get_session()
+    s.close()
+    tf.reset_default_graph()
+    print('done')
