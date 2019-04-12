f07418ed86c3bbb601bf299bf63ca9085ceec8ab
llach <llach@techfak.uni-bielefeld.de>
2019-04-12 17:49:15 +0200

    ppo2: added rl loss coefficient

--- macode/pipe/ppo2-pend-scratch.py
+++ macode/pipe/ppo2-pend-scratch.py
@@ -7,34 +7,34 @@ from baselines.run import main
 
 k = 5
 
-vae_params = {
-    'k': k,
-    'latent_dim': 5,
-    'beta': 1,
-}
+for rl_coef in [2, 10, 60]:
+    vae_params = {
+        'k': k,
+        'latent_dim': 5,
+        'beta': 1,
+    }
 
-def build_pend_env(args, **kwargs):
-    alg = args.alg
-    seed = args.seed
+    def build_pend_env(args, **kwargs):
+        alg = args.alg
+        seed = args.seed
 
-    flatten_dict_observations = alg not in {'her'}
-    env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                       flatten_dict_observations=flatten_dict_observations)
-    return VecFrameStack(env, k)
+        flatten_dict_observations = alg not in {'her'}
+        env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                           flatten_dict_observations=flatten_dict_observations)
+        return VecFrameStack(env, k)
 
-
-for seed in [0, 1, 2]:
     args = [
         '--env', 'PendulumVisual-v0',
-        '--num_timesteps', '30e6',
+        '--num_timesteps', '15e6',
         '--alg', 'ppo2',
         '--network', 'mlp',
         '--log_interval', '2',
         '--nminibatches', '32',
         '--noptepochs', '10',
         '--num_env', '16',
-        '--seed', str(seed),
+        '--seed', 0,
         '--tensorboard', 'True',
+        '--rl_coef', str(rl_coef),
         '--k', str(k),
     ]
 
