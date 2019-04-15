af9677e5fbb268a9ee3d2fcea21500275c63760a
llach <llach@techfak.uni-bielefeld.de>
2019-04-15 23:12:38 +0200

    ppo2 training on atari breakout

---
+++ macode/rl/ppo2-breakout.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+from baselines.common.tf_util import get_session
+from baselines.run import main
+
+args = [
+    '--env', 'BreakoutNoFrameskip-v4',
+    '--alg', 'ppo2',
+    '--num_env', '16',
+    '--log_interval', '1',
+    '--seed', '0',
+    '--tensorboard', 'True',
+]
+
+main(args)
+
+get_session().close()
+tf.reset_default_graph()
+
+print('done')
