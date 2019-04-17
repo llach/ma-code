f9756da3fe327a2905bb3559abc30ff9a931df14
llach <llach@techfak.uni-bielefeld.de>
2019-04-17 22:38:46 +0200

    ppo2-breakout: longer training

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -6,7 +6,7 @@ from baselines.run import main
 args = [
     '--env', 'BreakoutNoFrameskip-v4',
     '--alg', 'ppo2',
-    '--num_timesteps', '2e7',
+    '--num_timesteps', '10e7',
     '--num_env', '16',
     '--log_interval', '1',
     '--seed', '0',
