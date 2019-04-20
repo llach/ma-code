49ccc81141699ad27bc11c58496c66f878f2df37
llach <llach@techfak.uni-bielefeld.de>
2019-04-20 23:22:16 +0200

    clearing tf session

--- macode/vae/train/beta_search_atari.py
+++ macode/vae/train/beta_search_atari.py
@@ -1,7 +1,9 @@
+import tensorflow as tf
+
 from forkan.datasets import load_atari_normalized
 from forkan.models import VAE
 
-betas = [1.0, 1.28, 2.0, 3.5, 5.0]
+betas = [1.28, 2.0, 3.5, 5.0]
 
 for beta in betas:
     data = load_atari_normalized('breakout')
@@ -9,5 +11,7 @@ for beta in betas:
     v = VAE(data.shape[1:], network='atari', name='breakout', beta=beta, lr=1e-4, latent_dim=20)
     v.train(data, num_episodes=100, print_freq=200, batch_size=128)
 
+    v.s.close()
+    tf.reset_default_graph()
     del data
     del v
