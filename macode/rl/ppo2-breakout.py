9abcfe3edbbdae4c874699271765abea462cace4
llach <llach@techfak.uni-bielefeld.de>
2019-04-25 19:34:37 +0200

    more parameter testing

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -8,8 +8,6 @@ args = [
     '--alg', 'ppo2',
     '--num_timesteps', '1e7',
     '--num_env', '8',
-    '--ent_coef', '0.01',
-    '--network', 'cnn',
     '--log_interval', '1',
     '--seed', '0',
     '--tensorboard', 'True',
