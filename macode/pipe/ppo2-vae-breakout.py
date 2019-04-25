4be0598b19d3613b9fc6976c4391465072e5cde7
llach <llach@techfak.uni-bielefeld.de>
2019-04-25 16:26:26 +0200

    first ppo2 breakout training with pretrained VAE

---
+++ macode/pipe/ppo2-vae-breakout.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+
+from baselines.common.cmd_util import make_vec_env
+from baselines.common.tf_util import get_session
+from baselines.run import main
+from forkan.rl import VecVAEStack
+
+k = 4
+
+vae_names = [
+    'breakout-b1.0-lat20-lr0.0001-2019-04-20T18/00',
+]
+
+for seed in [0]:
+    for vae_name in vae_names:
+        def build_pend_env(args, **kwargs):
+            alg = args.alg
+            seed = args.seed
+
+            flatten_dict_observations = alg not in {'her'}
+            env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                                     flatten_dict_observations=flatten_dict_observations)
+            return VecVAEStack(env, k=k, load_from=vae_name.replace('/', ':'), vae_network='atari')
+
+        args = [
+            '--env', 'BreakoutNoFrameskip-v4',
+            '--num_timesteps', '1e7',
+            '--alg', 'ppo2',
+            '--network', 'mlp',
+            '--log_interval', '2',
+            '--nminibatches', '32',
+            '--noptepochs', '10',
+            '--num_env', '16',
+            '--seed', str(seed),
+            '--k', str(k),
+            '--tensorboard', 'True',
+            '--early_stop', 'True',
+        ]
+
+        main(args, build_fn=build_pend_env)
+        s = get_session()
+        s.close()
+        tf.reset_default_graph()
+        pass
