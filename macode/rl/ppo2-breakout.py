3232c6326edd48681bbb004d97f3eac8b2bfb6d8
llach <llach@techfak.uni-bielefeld.de>
2019-04-25 16:14:22 +0200

    parameter from issue to test

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -6,8 +6,10 @@ from baselines.run import main
 args = [
     '--env', 'BreakoutNoFrameskip-v4',
     '--alg', 'ppo2',
-    '--num_timesteps', '10e7',
-    '--num_env', '16',
+    '--num_timesteps', '1e7',
+    '--num_env', '8',
+    '--ent_coef', '0.01',
+    '--network', 'cnn',
     '--log_interval', '1',
     '--seed', '0',
     '--tensorboard', 'True',
