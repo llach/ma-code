009f958d057d05f820136672047ae43b1cc2ab62
llach <llach@techfak.uni-bielefeld.de>
2019-04-26 09:04:50 +0200

    using 16 worker

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -6,8 +6,8 @@ from baselines.run import main
 args = [
     '--env', 'BreakoutNoFrameskip-v4',
     '--alg', 'ppo2',
-    '--num_timesteps', '1e7',
-    '--num_env', '8',
+    '--num_timesteps', '10e6',
+    '--num_env', '16',
     '--log_interval', '1',
     '--seed', '0',
     '--tensorboard', 'True',
