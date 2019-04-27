9a02578074f4ad619c447cccd0b8a0f9bc42e901
llach <llach@techfak.uni-bielefeld.de>
2019-04-27 13:35:12 +0200

    breakout: old parameter

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -6,11 +6,10 @@ from baselines.run import main
 args = [
     '--env', 'BreakoutNoFrameskip-v4',
     '--alg', 'ppo2',
-    '--num_timesteps', '10e6',
     '--num_env', '16',
     '--log_interval', '1',
-    '--seed', '0',
     '--tensorboard', 'True',
+    '--seed', '0',
 ]
 
 main(args)
