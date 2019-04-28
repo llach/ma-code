b7852aa1ebb3f82ec35b8614d6f3e87839acbc1f
llach <llach@techfak.uni-bielefeld.de>
2019-04-28 10:30:07 +0200

    training ppo2 on pendulum ground truth

--- macode/rl/ppo2-pend.py
+++ macode/rl/ppo2-pend.py
@@ -5,29 +5,29 @@ from baselines.common.cmd_util import make_vec_env
 from baselines.common.tf_util import get_session
 from baselines.run import main
 
+for seed in [1, 2, 3]:
+    def build_pend_env(args, **kwargs):
+        return make_vec_env(args.env, 'classic_control', args.num_env or 1, args.seed, reward_scale=args.reward_scale,
+                                 flatten_dict_observations=True)
 
-def build_pend_env(args, **kwargs):
-    return make_vec_env(args.env, 'classic_control', args.num_env or 1, args.seed, reward_scale=args.reward_scale,
-                             flatten_dict_observations=True)
 
+    args = [
+        '--env', 'Pendulum-v0',
+        '--num_timesteps', '10e6',
+        '--alg', 'ppo2',
+        '--network', 'mlp',
+        '--nminibatches', '32',
+        '--noptepochs', '10',
+        '--num_env', '16',
+        '--log_interval', '2',
+        '--seed', str(seed),
+        '--tensorboard', 'True',
+        '--k', '5',
+    ]
 
-args = [
-    '--env', 'PendulumTheta-v0',
-    '--num_timesteps', '8e6',
-    '--alg', 'ppo2',
-    '--network', 'mlp',
-    '--nminibatches', '32',
-    '--noptepochs', '10',
-    '--num_env', '16',
-    '--log_interval', '2',
-    '--seed', '1',
-    '--tensorboard', 'True',
-    '--k', '5',
-]
-
-main(args)
-s = get_session()
-s.close()
-clear_session()
-tf.reset_default_graph()
-print('done')
+    main(args)
+    s = get_session()
+    s.close()
+    clear_session()
+    tf.reset_default_graph()
+    print('done')
