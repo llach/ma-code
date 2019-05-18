e84f9abf3da38ba101f247f115ef5e6cd3f9fec3
llach <llach@techfak.uni-bielefeld.de>
2019-05-18 13:50:07 +0200

    removed first trainings run

--- macode/pipe/ppo2-buffer-pend.py
+++ macode/pipe/ppo2-buffer-pend.py
@@ -7,43 +7,6 @@ from baselines.run import main
 
 k = 5
 
-vae_params = {
-    'k': k,
-    'latent_dim': 5,
-    'beta': 1,
-}
-
-def build_pend_env(args, **kwargs):
-    alg = args.alg
-    seed = args.seed
-
-    flatten_dict_observations = alg not in {'her'}
-    env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                       flatten_dict_observations=flatten_dict_observations)
-    return VecFrameStack(env, k)
-
-args = [
-    '--env', 'PendulumVisual-v0',
-    '--num_timesteps', '10e6',
-    '--alg', 'ppo_buffer',
-    '--network', 'mlp',
-    '--log_interval', '2',
-    '--nminibatches', '32',
-    '--noptepochs', '10',
-    '--num_env', '16',
-    '--seed', '0',
-    '--tensorboard', 'True',
-    '--rl_coef', str(10),
-    '--k', str(k),
-    '--log_interval', '1',
-]
-
-main(args, build_fn=build_pend_env, vae_params=vae_params)
-s = get_session()
-s.close()
-tf.reset_default_graph()
-print('done')
-
 for rl_coef in [30]:
     for es in ['True', 'False']:
         vae_params = {
@@ -72,12 +35,12 @@ for rl_coef in [30]:
             '--num_env', '16',
             '--seed', '0',
             '--tensorboard', 'True',
-            '--collect_until', '10e6',
+            '--collect_until', '1e7',
             '--rl_coef', str(rl_coef),
             '--k', str(k),
             '--log_interval', '1',
-            '--target_kl', '0.01',
-            '--early_stop', 'True',
+            # '--target_kl', '0.01',
+            # '--early_stop', 'True',
         ]
 
         main(args, build_fn=build_pend_env, vae_params=vae_params)
