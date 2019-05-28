f0a27ecbac3c617e004229d365ea92fdb5de74a9
llach <llach@techfak.uni-bielefeld.de>
2019-05-28 21:36:48 +0200

    vaecnn breakout

---
+++ macode/rl/ppo2-breakout-vaecnn.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+from baselines.common.tf_util import get_session
+from baselines.run import main
+
+args = [
+    '--env', 'BreakoutNoFrameskip-v4',
+    '--num_timesteps', '1e7',
+    '--network', 'cnn_atari',
+    '--alg', 'ppo2',
+    '--num_env', '16',
+    '--log_interval', '1',
+    '--tensorboard', 'True',
+    '--seed', '0',
+]
+
+main(args)
+
+get_session().close()
+tf.reset_default_graph()
+
+print('done')
