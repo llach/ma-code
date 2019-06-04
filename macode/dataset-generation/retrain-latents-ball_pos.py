92e70b56ce5589489d1819a128e9edc19e655b84
llach <llach@techfak.uni-bielefeld.de>
2019-06-04 11:04:52 +0200

    generating (latents, ball position) datasets; moved dataset generation scripts into own folder moved pipeline play scripts into own folder

---
+++ macode/dataset-generation/retrain-latents-ball_pos.py
@@ -0,0 +1,108 @@
+import numpy as np
+import tensorflow as tf
+
+from baselines.run import main
+
+from forkan.common.utils import ball_pos_from_rgb
+from gym.envs.classic_control import rendering
+from forkan.models import RetrainVAE
+from forkan import dataset_path
+
+policy_path = '/Users/llach/.forkan/done/stock-models/breakout-ppo/'
+vae_path = '/Users/llach/.forkan/done/breakout/ppo2-scratch/'
+vae_name = 'breakout-nenv16-rlc10000-k4-seed0-modelscratch-b1-2019-05-28T21:30/'
+vae_path += vae_name
+
+k = 4
+
+args = [
+    '--env', 'BreakoutNoFrameskip-v4',
+    '--alg', 'ppo2',
+    '--num_env', '1',
+    '--seed', str(0),
+    '--k', str(k),
+    '--load_path', policy_path,
+    '--play', 'True',
+]
+
+model, env = main(args, just_return=True)
+v = RetrainVAE(vae_path, (84, 84, 1), latent_dim=20, sess=tf.Session(), k=1, with_attrs=True, network='atari')
+v.s.run(tf.global_variables_initializer())
+v.load()
+
+viewer = rendering.SimpleImageViewer()
+
+obs = env.reset()
+d = False
+
+max_t = 1e3
+
+t = 0
+num_ep = 0
+
+# buffers to be saved
+org_buf = []
+rec_buf = []
+lat_buf = []
+pos_buf = []
+
+# working buffers for each episode
+preprocessed_frames = []
+
+while True:
+
+    obs_slice = obs[0, ..., -1]
+
+    img = env.render(mode='rgb_array')
+    ball_pos, fimg = ball_pos_from_rgb(img)
+
+    pos_buf.append(ball_pos)
+    preprocessed_frames.append(obs_slice/255)
+
+    # viewer.imshow(np.concatenate((img, fimg), axis=1))
+
+    action, _, _, _ = model.step(obs)
+    obs, _, done, _ = env.step(action)
+
+    t += 1
+
+    if done:
+        print(f'episode {num_ep} done at {t}; passing buffer into vae ...')
+
+        mu_t, logv_t = v.encode(np.asarray(preprocessed_frames, dtype=np.float32))
+        rec_buf.append(np.squeeze(v.decode(mu_t, logv_t)))
+
+        org_buf.append(preprocessed_frames.copy())
+        lat_buf.append(mu_t.copy())
+
+        preprocessed_frames = []
+
+        if t > max_t:
+            print(f'{t} > {max_t}. done collecting data samples.')
+            break
+
+        print('done, going on')
+
+        env.reset()
+        num_ep += 1
+        done = False
+
+org_buf = np.asarray(np.concatenate(org_buf, axis=0), dtype=np.float32)
+rec_buf = np.asarray(np.concatenate(rec_buf, axis=0), dtype=np.float32)
+lat_buf = np.asarray(np.concatenate(lat_buf, axis=0), dtype=np.float32)
+pos_buf = np.asarray(pos_buf, dtype=np.float32)
+
+print(f'sample size: {(org_buf.nbytes + rec_buf.nbytes + lat_buf.nbytes + pos_buf.nbytes)/org_buf.shape[0]/1000/1000}MB')
+print(f'dataset size: {(org_buf.nbytes + rec_buf.nbytes + lat_buf.nbytes + pos_buf.nbytes)/1000/1000}MB')
+
+print(org_buf.shape, rec_buf.shape, lat_buf.shape, pos_buf.shape)
+
+dataset_name = 'ball_latents_' + vae_name.split('-2019')[0] + '.npz'
+print(f'saving dataset {dataset_name} ...')
+
+np.savez_compressed(f'{dataset_path}/{dataset_name}',
+                    originals=org_buf, reconstructions=rec_buf, latents=lat_buf, ball_positions=pos_buf)
+
+viewer.close()
+
+print('dataset successfully generated!')
---
+++ macode/pipe/play/ppo2-breakout-scratch-play-gradients.py
@@ -0,0 +1,92 @@
+import numpy as np
+from scipy.misc import imresize
+
+from baselines.common.cmd_util import make_vec_env
+from baselines.common.vec_env.vec_frame_stack import VecFrameStack
+from baselines.run import main
+from baselines.common.tf_util import get_session
+from gym.envs.classic_control import rendering
+
+path = '/Users/llach/.forkan/done/breakout/ppo2-scratch/'
+path = '/Users/llach/.forkan/models/ppo2/'
+
+runs = [
+    'breakout-nenv16-rlc1-k4-seed0-modelscratch-b1-2019-05-28T16:40',
+    'breakout-nenv16-rlc10-k4-seed0-modelscratch-b1-2019-05-29T02:24',
+    'breakout-nenv16-rlc1000-k4-seed0-modelscratch-b1-2019-05-29T07:19',
+    'breakout-nenv16-rlc10000-k4-seed0-modelscratch-b1-2019-05-28T21:30',
+]
+
+k = 4
+
+vae_params = {
+    'k': k,
+    'latent_dim': 20,
+    'beta': 1,
+    'with_attrs': True,
+}
+
+def build_pend_env(args, **kwargs):
+    alg = args.alg
+    seed = args.seed
+
+    flatten_dict_observations = alg not in {'her'}
+    env = make_vec_env(args.env, 'atari', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                       flatten_dict_observations=flatten_dict_observations)
+    return VecFrameStack(env, k, norm_frac=255)
+
+
+args = [
+    '--env', 'BreakoutNoFrameskip-v4',
+    '--alg', 'ppo2',
+    '--network', 'mlp',
+    '--v_net', 'atari',
+    '--seed', str(0),
+    '--k', str(k),
+    '--load_path', f'{path}{runs[-1]}',
+    '--play', 'True',
+]
+
+model, env = main(args, build_fn=build_pend_env, vae_params=vae_params, just_return=True)
+# exit(0)
+obs = env.reset()
+d = False
+
+print('playing policy')
+
+viewer = rendering.SimpleImageViewer()
+
+t = 0
+num_ep = 0
+
+import tensorflow as tf
+vf_grad_op = tf.gradients(model.train_model.vf, model.vae.X)
+
+s = get_session()
+
+while num_ep < 3:
+
+    lob = np.expand_dims(np.squeeze(obs)[..., -1], -1)
+
+    vf_grad = s.run(vf_grad_op, feed_dict={model.vae.X: np.expand_dims(np.moveaxis(obs, -1, 1), -1)})
+    vf_grad = np.squeeze(np.asarray(vf_grad, dtype=np.float32))[-1]
+
+    # scale gradients to [0,1]
+    vf_grad += np.abs(np.min(vf_grad))
+    vf_grad *= (1 / (np.abs(np.min(vf_grad)) + np.max(vf_grad)))
+
+    lob = np.repeat(lob, 3, axis=-1)
+    lob[..., -1] += np.where(vf_grad > np.mean(vf_grad), vf_grad, np.zeros_like(vf_grad))
+
+    viewer.imshow(imresize(np.asarray(lob*255, dtype=np.uint8), (460, 320, 3)))
+
+    (actions, xhat, vf, dkl, recon, vl), x = model.step_xhat(obs)
+
+    obs, _, done, _ = env.step(actions)
+    d = done.any() if isinstance(done, np.ndarray) else done
+    t += 1
+    if d:
+        num_ep += 1
+        print(f'beginning episode {num_ep} at {t}')
+
+viewer.close()
