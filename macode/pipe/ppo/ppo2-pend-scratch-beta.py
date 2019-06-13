401643dda420d37cc0ca4b85d25163613762a3c7
llach <llach@techfak.uni-bielefeld.de>
2019-06-13 18:47:01 +0200

    removed early stopping loop

--- macode/pipe/ppo/ppo2-pend-scratch-beta.py
+++ macode/pipe/ppo/ppo2-pend-scratch-beta.py
@@ -7,42 +7,41 @@ from baselines.run import main
 
 k = 5
 
-for bet in [20, 5, 30, 60]:
-    for es in ['True', 'False']:
-        vae_params = {
-            'k': k,
-            'latent_dim': 5,
-            'beta': bet,
-        }
+for bet in [5, 30, 60]:
+    vae_params = {
+        'k': k,
+        'latent_dim': 5,
+        'beta': bet,
+    }
 
-        def build_pend_env(args, **kwargs):
-            alg = args.alg
-            seed = args.seed
+    def build_pend_env(args, **kwargs):
+        alg = args.alg
+        seed = args.seed
 
-            flatten_dict_observations = alg not in {'her'}
-            env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                               flatten_dict_observations=flatten_dict_observations)
-            return VecFrameStack(env, k)
+        flatten_dict_observations = alg not in {'her'}
+        env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
+                           flatten_dict_observations=flatten_dict_observations)
+        return VecFrameStack(env, k)
 
-        args = [
-            '--env', 'PendulumVisual-v0',
-            '--num_timesteps', '3e6',
-            '--alg', 'ppo2',
-            '--network', 'mlp',
-            '--log_interval', '2',
-            '--nminibatches', '32',
-            '--noptepochs', '10',
-            '--num_env', '16',
-            '--seed', '0',
-            '--plot_thetas', 'True',
-            '--tensorboard', 'True',
-            '--k', str(k),
-            '--target_kl', '0.01',
-            '--early_stop', es,
-        ]
+    args = [
+        '--env', 'PendulumVisual-v0',
+        '--num_timesteps', '3e6',
+        '--alg', 'ppo2',
+        '--network', 'mlp',
+        '--log_interval', '2',
+        '--nminibatches', '32',
+        '--noptepochs', '10',
+        '--num_env', '16',
+        '--seed', '0',
+        '--plot_thetas', 'True',
+        '--tensorboard', 'True',
+        '--k', str(k),
+        '--target_kl', '0.01',
+        '--early_stop', 'True',
+    ]
 
-        main(args, build_fn=build_pend_env, vae_params=vae_params)
-        s = get_session()
-        s.close()
-        tf.reset_default_graph()
-        print('done')
+    main(args, build_fn=build_pend_env, vae_params=vae_params)
+    s = get_session()
+    s.close()
+    tf.reset_default_graph()
+    print('done')
