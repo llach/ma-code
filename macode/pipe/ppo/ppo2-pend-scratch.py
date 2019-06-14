7386190c94a2e3af6f7cd76b17f0229fe46dab67
llach <llach@techfak.uni-bielefeld.de>
2019-06-14 08:47:00 +0200

    ppo2 pendulum scratch training script debloat

--- macode/pipe/ppo/ppo2-pend-scratch.py
+++ macode/pipe/ppo/ppo2-pend-scratch.py
@@ -13,40 +13,8 @@ vae_params = {
     'beta': 1,
 }
 
-def build_pend_env(args, **kwargs):
-    alg = args.alg
-    seed = args.seed
-
-    flatten_dict_observations = alg not in {'her'}
-    env = make_vec_env(args.env, 'classic_control', args.num_env or 1, seed, reward_scale=args.reward_scale,
-                       flatten_dict_observations=flatten_dict_observations)
-    return VecFrameStack(env, k)
-
-args = [
-    '--env', 'PendulumVisual-v0',
-    '--num_timesteps', '10e6',
-    '--alg', 'ppo2',
-    '--network', 'mlp',
-    '--log_interval', '2',
-    '--nminibatches', '32',
-    '--noptepochs', '10',
-    '--num_env', '16',
-    '--seed', '0',
-    '--tensorboard', 'True',
-    '--rl_coef', str(10),
-    '--k', str(k),
-    '--target_kl', '0.01',
-    '--early_stop', 'False',
-]
-
-main(args, build_fn=build_pend_env, vae_params=vae_params)
-s = get_session()
-s.close()
-tf.reset_default_graph()
-print('done')
-
-for rl_coef in [30]:
-    for es in ['True', 'False']:
+for rl_coef in [30, 10]:
+    for es in ['True']:
         vae_params = {
             'k': k,
             'latent_dim': 5,
