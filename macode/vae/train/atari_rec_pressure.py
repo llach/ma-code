08497ab6680d6caccd75a668a0a1b4d33ad4ee9b
llach <llach@techfak.uni-bielefeld.de>
2019-06-25 12:16:48 +0200

    higher zeta

--- macode/vae/train/atari_rec_pressure.py
+++ macode/vae/train/atari_rec_pressure.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from forkan.datasets import load_set
 from forkan.models import VAE
 
-zetas = [2, 5, 10, 20, 30]
+zetas = [100, 1000, 10000]
 
 for zeta in zetas:
     data = load_set('breakout-normalized-small')
