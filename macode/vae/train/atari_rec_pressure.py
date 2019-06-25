8acb62e5ee548d66772eef1fe53dd71541e03cd9
llach <llach@techfak.uni-bielefeld.de>
2019-06-25 11:00:34 +0200

    vae logging images; increasing pressure on reconstruction

---
+++ macode/vae/train/atari_rec_pressure.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+from forkan.datasets import load_set
+from forkan.models import VAE
+
+zetas = [2, 5, 10, 20, 30]
+
+for zeta in zetas:
+    data = load_set('breakout-normalized-small')
+
+    v = VAE(data.shape[1:], network='atari', name='breakout', zeta=zeta, lr=1e-4, latent_dim=20)
+    v.train(data, num_episodes=50, print_freq=100, batch_size=128)
+
+    v.s.close()
+    tf.reset_default_graph()
+    del data
+    del v
