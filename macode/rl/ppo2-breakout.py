c50a609efbd874921414f77591a6a236effb6c9b
llach <llach@techfak.uni-bielefeld.de>
2019-06-26 11:09:45 +0200

    breakout baseline with different seeds

--- macode/rl/ppo2-breakout.py
+++ macode/rl/ppo2-breakout.py
@@ -3,19 +3,22 @@ import tensorflow as tf
 from baselines.common.tf_util import get_session
 from baselines.run import main
 
-args = [
-    '--env', 'BreakoutNoFrameskip-v4',
-    '--num_timesteps', '1e7',
-    '--alg', 'ppo2',
-    '--num_env', '16',
-    '--log_interval', '1',
-    '--tensorboard', 'True',
-    '--seed', '0',
-]
+for seed in [0, 1, 2]:
+    args = [
+        '--env', 'BreakoutNoFrameskip-v4',
+        '--num_timesteps', '1e7',
+        '--alg', 'ppo2',
+        '--num_env', '16',
+        '--nminibatches', '32',
+        '--noptepochs', '10',
+        '--log_interval', '1',
+        '--tensorboard', 'True',
+        '--seed', str(seed),
+    ]
 
-main(args)
+    main(args)
 
-get_session().close()
-tf.reset_default_graph()
+    get_session().close()
+    tf.reset_default_graph()
 
 print('done')
