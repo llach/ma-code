21c6ec55a19ecd1f8431c7b6b11e4837e10c9c3f
llach <llach@techfak.uni-bielefeld.de>
2019-07-02 12:46:57 +0200

    some plotting things

---
+++ macode/shiny/breakout/ppo2-fixed-adapt.py
@@ -0,0 +1,39 @@
+import logging
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from forkan.common.utils import read_keys, setup_plotting, get_figure_size
+
+
+logger = logging.getLogger(__name__)
+
+ylims, tick_setup = setup_plotting('break-baseline')
+fig, ax = plt.subplots(1, 1, figsize=get_figure_size())
+
+home = os.environ['HOME']
+models_dir = f'{home}/.forkan/done/breakout/ppo2-break-fixed'
+
+for fi, name in [('b1', '$PPO^{fixed}$')]:
+    data = read_keys(models_dir, fi, ['mean_reward', 'total_timesteps'])
+
+    xs = data['total_timesteps'][0]
+    ys = data['mean_reward']
+
+    plt.plot(xs, np.nanmedian(ys, axis=0), label=name)
+    plt.fill_between(xs, np.nanpercentile(ys, 25, axis=0), np.nanpercentile(ys, 75, axis=0), alpha=0.33)
+
+plt.ylim(**ylims)
+
+ax.set_ylabel('Median Reward')
+ax.set_xlabel('Steps')
+plt.xticks(tick_setup[0], tick_setup[1])
+ax.legend(loc='center right')
+
+fig.tight_layout()
+
+plt.savefig(f'{models_dir}/fixed-adapt.pdf')
+plt.show()
+
+logger.info('Done.')
--- macode/shiny/pendulum/ppo2-scratch.py
+++ macode/shiny/pendulum/ppo2-scratch.py
@@ -13,7 +13,7 @@ ylims, tick_setup = setup_plotting()
 fig, ax = plt.subplots(1, 1, figsize=get_figure_size())
 
 home = os.environ['HOME']
-models_dir = f'{home}/.forkan/done/pendulum/ppo2-scratch'
+models_dir = f'{home}/.forkan/done/pendulum/ppo2-scratch-pend-clean'
 
 # for fi, name in [('rlc1-k5-seed0', 'kappa=1'), ('rlc10-k5-seed0', 'kappa=10'), ('rlc30-k5-seed0', 'kappa=30')]:
 #     data = read_keys(models_dir, fi, ['mean_reward', 'nupdates'])
@@ -53,11 +53,11 @@ plt.ylim(**ylims)
 ax.set_ylabel('Median Reward')
 ax.set_xlabel('Steps')
 plt.xticks(tick_setup[0], tick_setup[1])
-ax.legend(loc='center right')
+ax.legend(loc='lower right')
 
 fig.tight_layout()
 
-plt.savefig(f'{home}/.forkan/done/pendulum/ppo2-scratch/kappa-stop.pdf')
+plt.savefig(f'{home}/.forkan/done/pendulum/ppo2-scratch-pend-clean/kappa-stop.pdf')
 plt.show()
 
 logger.info('Done.')
